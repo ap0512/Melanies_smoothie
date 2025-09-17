d84eb2d40193a4c5fb4d5aa5eb33f13277c2449f
ap0512 <143703419+ap0512@users.noreply.github.com>
2025-09-17 19:15:57 +0530

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,5 +1,7 @@
 # Import python packages
 import streamlit as st
+import requests
+
 # Write directly to the ap
 st.title(f"Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -24,17 +26,14 @@ ingredients_list = st.multiselect(
 'Choose up to 5 ingredients:',
 my_dataframe)
 
-
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json(), use_container_width = True)
-
 if ingredients_list:
 
-    
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st_ddf = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
+
         # st.write (ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
